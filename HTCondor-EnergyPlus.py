0502ff3f48f9cb8ed8df1963771244668b090fed
Jeremy <jeremy.lerond@gmail.com>
2016-02-08 11:55:11 -0800

    Readme

--- HTCondor-EnergyPlus.py
+++ HTCondor-EnergyPlus.py
@@ -1,4 +1,30 @@
 import CreateBatch
+import pyforms
+import sys
+from pyforms import BaseWidget
+from pyforms.Controls import ControlText
+from pyforms.Controls import ControlButton
+from pyforms.Controls import ControlFile
 
-CSVFile = "JobTemplate.csv"
-CreateBatch.CreateBatch(CSVFile)
+class HTCondorEnergyPlus(BaseWidget):
+	
+	def __init__(self):
+		super(HTCondorEnergyPlus,self).__init__('HTCondor & EnergyPlus')
+
+		self._CSVFile 	= ControlFile('CSV File')
+		self._Create	= ControlButton('Create')
+		self._Submit	= ControlButton('Submit')
+		self._Close		= ControlButton('Close')
+
+		self._formset = ['',('  ','_CSVFile','  '),('', '_Create', '_Submit','' ),('','_Close',''),'']
+	
+		self._Create.value = self.__CreateAction
+		self._Close.value = self.__CloseAction
+	
+	def __CreateAction(self):
+		CreateBatch.CreateBatch(self._CSVFile.value)
+	
+	def __CloseAction(self):
+		sys.exit()
+
+if __name__ == "__main__":	 pyforms.startApp( HTCondorEnergyPlus )
---
+++ README.md
@@ -0,0 +1,5 @@
+# HTCondor-EnergyPlus
+
+Simple GUI written in Python to run EnergyPlus simulations on a HTCondor pool using a simple csv file as input.
+
+Not ready for use yet...
